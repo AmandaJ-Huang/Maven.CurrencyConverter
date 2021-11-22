e690f4935332264dd486a0543154b3a78638b1c5
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-21 22:43:35 -0500

    completed ausDollar

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,16 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
+
+import java.util.Currency;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+    @Override
+    public Double convert(CurrencyType currencyType) {
+
+        return (CurrencyType.UNIVERSAL_CURRENCY.getRate()/CurrencyType.AUSTRALIAN_DOLLAR.getRate())
+                * currencyType.getRate();
+    }
 }
